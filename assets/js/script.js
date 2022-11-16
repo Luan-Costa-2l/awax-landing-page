d792de37d0de9a5f5e6a436a034d3a873f767654
Luan Cordeiro <luam.lc19@gmail.com>
2022-11-15 22:14:02 -0300

    Continuando a responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,32 @@ const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
 // Functions
+function handleMenu() {
+    let left = parseInt(c('header .menu .menu--icon').getAttribute('data-show'));
+
+    if(left == 0) {
+        c('header .menu .menu--icon').setAttribute('data-show') = 1;
+        c('header .menu nav').style.left = '-60vw';
+    } else {
+        lc('header .menu .menu--icon').setAttribute('data-show') = 0;
+        c('header .menu nav').style.left = '0';
+    }
+}
+
 function changeItem(e) {
     c('a.active').classList.remove('active');
     e.target.classList.add('active');
 }
 
 // Events
+c('header .menu--icon').addEventListener('click', handleMenu);
+
 cs('header nav ul a').forEach((item) => {
     item.addEventListener('click', changeItem);
 });
 
 cs('.banner .pointer').forEach((item) => {
-    item.addEventListener('click', (e) => {
+    item.addEventListener('click', () => {
         c('.banner .pointer.active').classList.remove('active');
         item.classList.add('active');
 
